71690b9ae6025a1cd11ad46bf97c05ff192cbd4b
Paweł Wrona <pawelwrona2009@gmail.com>
2026-02-20 10:16:46 +0100

    Basic endpionts

--- scr/index.js
+++ scr/index.js
@@ -1,11 +1,42 @@
 import express from "express";
-const app = express()
-const port = 3008
+import { error } from "node:console";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+
+app.use(express.json());
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message,
+  });
+});
+
+const port = 3005;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/movies", (req, res) => {
+  const movies = [
+    {
+      title: "Avatar",
+      director: "James Cameron",
+    },
+    {
+      title: "Interstellar",
+      director: "XYZ",
+    },
+  ];
+
+  res.json(movies);
+});
+
+app.post("/api/movies", (req, res) => {
+  const movie = req.body;
+
+  res.status(201).json({ message: "Dodano film", ...movie });
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port} on port ${port}`);
-})
+});
